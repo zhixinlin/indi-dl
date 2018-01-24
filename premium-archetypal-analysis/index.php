dbd01c02df2146abc3290215b2268d65f10c7ecb
zhixinlin <linzedex@gmail.com>
2018-01-24 19:02:46 +0800

    upload

---
+++ premium-archetypal-analysis/index.php
@@ -0,0 +1,137 @@
+<?php
+$PageTitle = "Individualogist.com | Download Area | Premium Archetypal Analysis";
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= ('/indi-dl/assets/inc/header.php');
+include_once($path);
+?>
+<body>
+<div class="dl-container">
+    
+        <div class="indi-logo">
+            <img src="/sp/assets/img/individualogist-logo2.png" width="300px">
+        </div>
+        
+    <div class="dl-header">
+        <h1>Download Area: Premium Archetypal Analysis</h1>
+        <h3>Archetype: <span aweber-field="archetype">Archetype</span></h3>
+    </div>
+
+    <div class="dl-content">
+        <p>Dear <span aweber-field="name">Friend</span>,</p>
+
+        <p>Thank you for purchasing your copy of the Premium Archetypal Analysis!</p>
+
+        <p>We have worked incredibly hard to create this comprehensive report, and we do hope that you enjoy it!</p>
+
+        <p>But before you begin, <span aweber-field="name">Friend</span>, please go through this short instructional that will tell you exactly where to begin, and what to expect.</p>
+
+        <p>The Premium Archetypal Analysis has been segmented into 5 primary portions:</p>
+
+        <p>1. Individuation Fundamentals</p>
+        <p>2. Archetypal Profile</p>
+        <p>3. Archetypal Incarnations</p>
+        <p>4. Archetypal Odyssey</p>
+        <p>5. Archetypal Exercise Guide</p>
+
+        <p>Now, here’s what I want you to do, <span aweber-field="name">Friend</span>.</p>
+
+        <p><span class="purple">Step 1:</span> Please bookmark this link in case you lose the e-mail that was sent to you earlier. You could also write down the URL of this page on a piece of paper and keep it somewhere safe! Are you finished, Friend? Perfect!</p>
+
+        <p><span class="purple">Step 2:</span> Scroll down and right click on the “Download Now” button for all 5 segments of the Premium Archetypal Analysis, and select “Save Link As”. This will store the entire series into your computer (if you’re accessing this from a mobile device, you can simply tap on the “Download Now” button instead).</p>
+
+        <p><span class="purple">Step 3:</span>: Now, please go through the various portions of the Premium Archetypal Analysis in the following order: Individuation Fundamentals, Archetypal Profile, Archetypal Incarnations, Archetypal Odyssey, and finally, the Archetypal Exercise Guide. Below are the descriptions of each portion of the Premium Archetypal Analysis. And if you scroll down even further, you will find the additional 3 bonus eBooks.</p>
+
+        <p>Now, without any further delay, let’s get started! <span aweber-field="name">Friend</span>, are you ready?</p>
+    </div>
+    
+    <div id="dl-wrapper">
+        <div class="dl-sub-header">
+            <h3>Premium Archetypal Analysis: <span awe-berfield="archetype">Archetype</span></h3>
+        </div>
+        
+        <div class="dl-col">
+            <p>Reading #1: Individuation Fundamentals</p>
+            <div class="dl-img">
+                <img src="../assets/img/paa-fundamentals.png" width="70%">
+            </div>
+            <p>The Individuation Fundamentals PDF will tell you more about the individuation journey, and introduce to you a few “new” terminologies that you’ll encounter throughout your rest of the Premium Archetypal Analysis. This reading will familiarize you with the Shadow, the Anima/Animus, and other related entities. Learning about these terminologies and ideas beforehand will greatly enhance your learning experience!</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/premium-archetypal-analysis/car/Individuation+Fundamentals+-+Caregiver.pdf" download target="_blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Reading #2: Archetypal Profile</p>
+            <div class="dl-img">
+                <img src="../assets/img/paa-profile.png" width="70%">
+            </div>
+            <p>The Archetypal Profile PDF is where the “meat” of the Premium Archetypal Analysis lies; where you’ll be introduced to your archetype at a more profound level. Do note that certain parts of the profile will require some interpretation, and that’s up to your own personal understanding of who you are as an individual. Approach the Archetypal Profile will an open-mind, and I assure you that you will receive the value that you seek.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/premium-archetypal-analysis/car/Archetypal+Profile+-+Caregiver.pdf" download target="_blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Reading #3: Archetypal Incarnations</p>
+            <div class="dl-img">
+                <img src="../assets/img/paa-incarnations.png" width="70%">
+            </div>
+            <p>The Archetypal Incarnations PDF introduces to you several sub-archetypes, or incarnations of your dominant archetype. Each dominant archetype comprises a number of incarnations that forms its entirety. Learning about your incarnations will reveal some of your thought processes and personality traits. Depending on your background and upbringing, certain incarnations might or might not resonate with you.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/premium-archetypal-analysis/car/Archetypal+Incarnations+-+Caregiver.pdf" download target="_blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Reading #4: Archetypal Odyssey</p>
+            <div class="dl-img">
+                <img src="../assets/img/paa-odyssey.png" width="70%">
+            </div>
+            <p>Within the Archetypal Odyssey, you will find the ideal journey that your archetype is meant to embark on, the obstacles that you will potentially face, and what you can do to overcome those obstacles. It will reveal to you the direction that you should be headed towards, based on your archetypal profile and archetypal incarnations.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/premium-archetypal-analysis/car/Archetypal+Odyssey+-+Caregiver.pdf" download target="_blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Reading #5: Archetypal Exercise Guide</p>
+            <div class="dl-img">
+                <img src="../assets/img/paa-exercise-guide.png" width="70%">
+            </div>
+            <p>The Archetypal Exercise Guide is the final step of the Premium Archetypal Analysis, where you will translate everything you learned into action. Within this guide, you will find a series of exercises that will solidify the knowledge that you have acquired. By the end of this guide, your archetype will be brought to consciousness!</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/bonuses/Archetypal+Exercise+Guide.pdf" download target="_blank">Download Now</a>
+        </div>
+    </div>
+    
+    <div id="dl-wrapper">
+        <div class="dl-sub-header">
+            <h3>Additional Bonuses</h3>
+        </div>
+        
+        <div class="dl-col">
+            <p>Bonus #1: Exploring Your Birthdate With The Chinese Zodiac</p>
+            <div class="dl-img">
+                <img src="../assets/img/chinese-zodiac-cover.png" width="70%">
+            </div>
+            <p>Meet the counterpart of Western astrology – The Chinese Zodiac. Understand the Eastern influences on predictions and learn how you can apply them to predict your own future.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/bonuses/Exploring+Your+Birthdate+With+The+Chinese+Zodiac.pdf" download target="_blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Bonus #2: Discovering Your Aura And What It Says About You</p>
+            <div class="dl-img">
+                <img src="../assets/img/discovering-aura-cover.png" width="70%">
+            </div>
+            <p>Each individual exudes a certain aura. Learn how you can interpret different auras of different individuals to gain maximum influence over others.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/bonuses/Discovering+Your+Aura.pdf" download target="blank">Download Now</a>
+        </div>
+        
+        <div class="dl-col">
+            <p>Bonus #3: Beginner's Guide To The Feng Shui Paradigm</p>
+            <div class="dl-img">
+                <img src="../assets/img/fengshui-paradigm-cover.png" width="70%">
+            </div>
+            <p>Maximize the flow of energy in your home or office by discovering a few simple techniques from the Eastern art of Fengshui. This in-depth guide will turn you into a true geomancy master in no time.</p>
+            <a class="dl-button" href="https://s3.us-east-2.amazonaws.com/individua1/bonuses/Beginner's+Guide+To+Feng+Shui.pdf" download target="blank">Download Now</a>
+        </div>
+    </div>
+    
+</div>
+    
+<?php 
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= ('/indi-dl/assets/inc/footer.php');
+include_once($path); 
+?>
